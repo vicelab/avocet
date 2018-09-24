acc6fb568d5646d7177d878ab23ebcb0033148fb
Brian <brianhungerman@gmail.com>
2018-09-23 17:46:57 -0700

    Rasterizing TIFF inputs

--- main.R
+++ main.R
@@ -1,20 +1,19 @@
+
 library(ggplot2)
 library(raster)
-#D:\\IMAGES\\AVO_Images\\AVO_PScope4\\WY2018_AVO_PScope4\\images\\use\\20171002_180525_0f28_3B_AnalyticMS.tif
-data(lsat)
-lsat
+library (tiff)
+library("RStoolbox")
 
+setwd("D:\\IMAGES\\AVO_Images\\AVO_PScope4\\WY2018_AVO_PScope4\\images\\use");
+val<-'20171002_180525_0f28_3B_AnalyticMS.tif'
+read_file=raster(val)
+read_file
+read_brick = brick(read_file)
+read_brick
 ## Calculate NDVI
-ndvi <- spectralIndices(lsat, red = "B3_dn", nir = "B4_dn", indices = "NDVI")
+ndvi <- spectralIndices(read_brick, red = "X20171002_180525_0f28_3B_AnalyticMS",nir = "X20171002_180525_0f28_3B_AnalyticMS", indices = "NDVI")
 ndvi
-ggR(ndvi, geom_raster = TRUE) +
-  scale_fill_gradientn(colours = c("black", "white")) 
-mtlFile  <- system.file("external/landsat/LT52240631988227CUB02_MTL.txt", package="RStoolbox")
-lsat_ref <- radCor(lsat, mtlFile, method = "apref")
-
-SI <- spectralIndices(lsat_ref, red = "B3_tre", nir = "B4_tre")
-plot(SI)
-
+plot(ndvi)
 
 
 
