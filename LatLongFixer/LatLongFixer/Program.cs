aad53ea91ef29f89432686b8f228db405e94fb9b
Brian <brianhungerman@gmail.com>
2018-09-24 20:50:02 -0700

    Replaced LatLong testbed with console app (lighter weight)

---
+++ LatLongFixer/LatLongFixer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LatLongFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            intro();
+        }
+
+        static void intro()
+        {
+            Console.Clear();
+            string filler = "";
+            for (int i = 0; i < 50 ; i++)
+            {
+                filler += "~";
+            }
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Welcome to the LatLongFixer, Patented by Brian ~");
+            Console.WriteLine(filler);
+            Console.ReadLine();
+        }
+    }
+}
