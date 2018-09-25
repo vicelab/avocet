1bc745a0c970708f6634aca1498f2f7a399a30bc
Brian <brianhungerman@gmail.com>
2018-09-24 19:46:04 -0700

    Generalized code for more applications
    Supports predefined file types, indices, folder locations, etc.

--- main.R
+++ main.R
@@ -4,26 +4,33 @@ library(raster)
 library (tiff)
 library(RStoolbox)
 
-## Grab A File
-setwd("D:\\IMAGES\\AVO_Images\\AVO_PScope4\\WY2018_AVO_PScope4\\images\\use");
+folder = "D:\IMAGES\AVO_Images\AVO_PScope4\WY2018_AVO_PScope4\images\use"
+file_type = ".tif"
+plot_type = "NDVI"
 
-files <- list.files(path=getwd(), pattern="*.tif", full.names=TRUE, recursive=FALSE)
-lapply(files, function(x) {
-  #'20171002_180525_0f28_3B_AnalyticMS'
-  x = substr(x, (nchar(getwd())+2), 1000)
-  x = substr(x, 0, (nchar(x) - 4))
- # sapply(strsplit(x, "."), "[", 1)
-  x
-  val<- x
-  read_brick=brick(paste(val, "tif", sep="."))
+## Enter A Folder
+setwd(folder);
+
+# Iterate over every File in Folder with desired File Type
+files <- list.files(path=getwd(), pattern=paste("*",file_type, sep=""), full.names=TRUE, recursive=FALSE)
+lapply(files, function(file) {
+  # Trim the file to just the File Name
+  # e.g. "D:\IMAGES\AVO_Images\AVO_PScope4\WY2018_AVO_PScope4\images\use\20171002_180525_0f28_3B_AnalyticMS.tif"
+  # goes to "20171002_180525_0f28_3B_AnalyticMS"
+  file = substr(file, (nchar(getwd())+2), 1000)
+  file = substr(file, 0, (nchar(file) - 4))
+  
+  # RasterBrick-ize file
+  file_brick=brick(paste(file, file_type, sep=""))
   
   ## Calculate NDVI
-  ndvi <- spectralIndices(read_brick, red = paste("X",val, ".3", sep=""), nir = paste("X",val, ".4", sep=""), indices = "NDVI")
+  # Uses Bands 3 (RED) and 4 (NIR)
+  ndvi <- spectralIndices(file_brick, red = paste("X",file, ".3", sep=""), nir = paste("X",file, ".4", sep=""), indices = plot_type)
   
-  ## Plot NDVI
-  pdf(paste(x,".pdf", sep=""))
+  ## Plot NDVI to PDF
+  pdf(paste(file,plot_type,".pdf", sep=""))
   plot(ndvi)
-   dev.off()
+  dev.off()
 })
 
 
