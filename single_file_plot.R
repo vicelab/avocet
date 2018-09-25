85c5d4892ab6ff9728ca04a15c09913b4b94000f
Brian <brianhungerman@gmail.com>
2018-09-24 20:29:14 -0700

    Added processing for single files through new R script

---
+++ single_file_plot.R
@@ -0,0 +1,40 @@
+######################################################
+#                                                    #
+# Author: Brian Hungerman                            #
+# Date  : 9/24/2018                                  #
+# Affil.: VICE Lab, University of California, Merced #
+#                                                    #
+######################################################
+
+## Necessary Libraries
+# If libraries aren't installed on your device, uncomment the following 4 lines
+# install.packages("ggplot2")
+# install.packages("raster")
+# install.packages("tiff")
+# install.packages("RStoolbox")
+library(ggplot2)
+library(raster)
+library(tiff)
+library(RStoolbox)
+
+##Edit these parameters
+folder = "D:\\IMAGES\\AVO_Images\\AVO_PScope4\\WY2017_AVO_PScope4\\images\\use"
+file = "20161105_190326_0c75_3B_AnalyticMS"
+file_type = ".tif"
+plot_type = "NDWI"
+
+## Enters defined Folder
+setwd(folder);
+
+## RasterBrick-ize File
+file_brick=brick(paste(file, file_type, sep=""))
+  
+## Calculate Indices
+# Uses Bands 3 (RED) and 4 (NIR)
+output <- spectralIndices(file_brick, blue = paste("X",file, ".1", sep=""), green = paste("X",file, ".2", sep=""),red = paste("X",file, ".3", sep=""), nir = paste("X",file, ".4", sep=""), indices = plot_type)
+  
+## Plot to PDF
+pdf(paste(file,"-",plot_type,".pdf", sep=""))
+plot(output)
+dev.off()
+
