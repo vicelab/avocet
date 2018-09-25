d17585c157a6218aff0b38ec000fe993fafbc700
Brian <brianhungerman@gmail.com>
2018-09-24 19:06:53 -0700

    Spidering NDVI Code
    Runs through all files in a provided directory, outputs a unique pdf with NVDI calculations

--- main.R
+++ main.R
@@ -2,18 +2,30 @@
 library(ggplot2)
 library(raster)
 library (tiff)
-library("RStoolbox")
+library(RStoolbox)
 
+## Grab A File
 setwd("D:\\IMAGES\\AVO_Images\\AVO_PScope4\\WY2018_AVO_PScope4\\images\\use");
-val<-'20171002_180525_0f28_3B_AnalyticMS.tif'
-read_file=raster(val)
-read_file
-read_brick = brick(read_file)
-read_brick
-## Calculate NDVI
-ndvi <- spectralIndices(read_brick, red = "X20171002_180525_0f28_3B_AnalyticMS",nir = "X20171002_180525_0f28_3B_AnalyticMS", indices = "NDVI")
-ndvi
-plot(ndvi)
+
+files <- list.files(path=getwd(), pattern="*.tif", full.names=TRUE, recursive=FALSE)
+lapply(files, function(x) {
+  #'20171002_180525_0f28_3B_AnalyticMS'
+  x = substr(x, (nchar(getwd())+2), 1000)
+  x = substr(x, 0, (nchar(x) - 4))
+ # sapply(strsplit(x, "."), "[", 1)
+  x
+  val<- x
+  read_brick=brick(paste(val, "tif", sep="."))
+  
+  ## Calculate NDVI
+  ndvi <- spectralIndices(read_brick, red = paste("X",val, ".3", sep=""), nir = paste("X",val, ".4", sep=""), indices = "NDVI")
+  
+  ## Plot NDVI
+  pdf(paste(x,".pdf", sep=""))
+  plot(ndvi)
+   dev.off()
+})
+
 
 
 
