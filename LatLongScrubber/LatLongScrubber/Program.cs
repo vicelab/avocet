ead8edfae8df624d848692c7d2a57692e5d22fb7
Brian <brianhungerman@gmail.com>
2018-09-27 16:24:09 -0700

    UI/UX Improvements

--- LatLongScrubber/LatLongScrubber/Program.cs
+++ LatLongScrubber/LatLongScrubber/Program.cs
@@ -6,9 +6,10 @@ namespace LatLongFixer
 {
     class Program
     {
+        static int response = 0;
         static void Main(string[] args)
         {
-            intro();
+            while(true) intro();
         }
 
         static void intro()
@@ -22,60 +23,42 @@ namespace LatLongFixer
             Console.WriteLine(filler);
             Console.WriteLine("~ Welcome to the LatLongFixer, Patented by Brian ~");
             Console.WriteLine(filler);
-          //  Console.ReadLine();
-            string[] lines = File.ReadAllLines(@"input.txt");
-            for (int i = 0; i < lines.Length; i += 50)
+            Console.WriteLine("~ Enter file location and press ENTER.\n~ (e.g. C:/data/log.txt )");
+
+            string file_location = Console.ReadLine();
+
+            //  Console.ReadLine();
+            string[] lines = File.ReadAllLines(file_location);//@"input.txt");
+
+            for (int i = 0; i < lines.Length; i += 100)
             {
                 Console.WriteLine(lines[i]);
             }
             Console.Clear();
 
             Console.WriteLine(filler);
-            Console.WriteLine("~  Thank you for inputting a file, Parsing Now!  ~");
+            Console.WriteLine("~ Welcome to the LatLongFixer, Patented by Brian ~");
             Console.WriteLine(filler);
-          //  Console.ReadLine();
-            string[] line;
-            for (int i = 0; i < lines.Length; i += 50)
-            {
-                line = lines[i].Split('	');
-                Console.WriteLine("Pos: " + line[1] + "," + line[2]);
-            }
-            Console.Clear();
+            Console.WriteLine("~ Reading from: " + file_location);
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Enter distance threshold and press ENTER.\n~ (e.g.  0.0000000001)");
+            Console.WriteLine(filler);
+            double threshold = double.Parse(Console.ReadLine());
+
             string[] prev_line;
-            double[] running_average = new double[] { 0, 0 };
-            for (int i = 1; i < lines.Length; i += 1)
-            {
-                prev_line = lines[i - 1].Split('	');
-                line = lines[i].Split('	');
-                //Give some haptic feedback
-                running_average[0] += Math.Abs(double.Parse(line[1]) - double.Parse(prev_line[1]));
-                running_average[1] += Math.Abs(double.Parse(line[2]) - double.Parse(prev_line[2]));
-
-                if (i % 50 == 0) Console.WriteLine("ΔPos: " + running_average[0] + "," + running_average[1]);
-            }
-            Console.Clear();
-            running_average[0] = running_average[0] / lines.Length;
-            running_average[1] = running_average[1] / lines.Length;
-            Console.WriteLine(filler);
-            Console.WriteLine("~  Average Distance Found, Identifying problems  ~");
-            Console.WriteLine(filler);
-            Console.WriteLine(running_average[0] / lines.Length + "," + running_average[1] / lines.Length);
-         //   Console.ReadLine();
+            string[] line;
 
             for (int i = 1; i < lines.Length; i += 1)
             {
                 prev_line = lines[i - 1].Split('	');
                 line = lines[i].Split('	');
-                //if (double.Parse(line[1]) - double.Parse(prev_line[1]) > running_average[0]*2 || double.Parse(line[2]) - double.Parse(prev_line[2]) > running_average[1]*2)
                 if (Math.Abs(double.Parse(line[1]) - double.Parse(prev_line[1])) < 0.0000000001)
                 {
                     Console.WriteLine("Line[" + i + "] is messed up?");
                 }
             }
-            Console.Clear();
-            Console.WriteLine(filler);
-            Console.WriteLine("~  Rewriting .TXT with removed points. . . . . . ~");
-            Console.WriteLine(filler);
+
+
             string output = "";
             for (int i = 1; i < lines.Length; i += 1)
             {
@@ -95,18 +78,52 @@ namespace LatLongFixer
                 {
                     Console.Clear();
                     Console.WriteLine(filler);
-                    Console.WriteLine("~  Rewriting .TXT with removed points. . . . . . ~");
+                    Console.WriteLine("~ Welcome to the LatLongFixer, Patented by Brian ~");
                     Console.WriteLine(filler);
-                    Console.WriteLine(((double)i / lines.Length).ToString("p") + " the way there!");
+                    Console.WriteLine("~ Reading from: " + file_location);
+                    Console.WriteLine(filler);
+                    Console.WriteLine("~ Using threshold: " + threshold);
+                    Console.WriteLine(filler);
+                    Console.WriteLine("~ Rewriting .TXT with removed points.");
+                    Console.WriteLine(filler);
+                    Console.WriteLine("~ " + ((double)i / lines.Length).ToString("p") + " the way there!");
                 }
             }
             Console.Clear();
             Console.WriteLine(filler);
-            Console.WriteLine("~  Saving File, Program will Close when Done     ~");
+            Console.WriteLine("~ Welcome to the LatLongFixer, Patented by Brian ~");
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Reading from: " + file_location);
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Using threshold: " + threshold);
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Rewrote .TXT with removed points.");
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Overriding file");
             Console.WriteLine(filler);
 
-            File.WriteAllText(@"output.txt", output);
+            File.WriteAllText(file_location, output);
 
+            Console.Clear();
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Welcome to the LatLongFixer, Patented by Brian ~");
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Reading from: " + file_location);
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Using threshold: " + threshold);
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Rewrote .TXT with removed points.");
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Overriding file");
+            Console.WriteLine(filler);
+            Console.WriteLine("~ Enter 1 to rerun process. Press 0 to close.");
+
+            response = int.Parse(Console.ReadLine());
+
+            if (response == 0)
+            {
+                Environment.Exit(0);
+            }
         }
     }
 }
