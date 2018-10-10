caa145a74c9f986ea9c77d370b33ddfc0515fc5d
Brian <brianhungerman@gmail.com>
2018-10-10 11:16:35 -0700

    Reorganization

--- SpectralIndices/multifile_plot.R
+++ SpectralIndices/multifile_plot.R
@@ -18,9 +18,17 @@ library(tiff)
 library(RStoolbox)
 
 ##Edit these parameters
-folder = "D:\\IMAGES\\AVO_Images\\AVO_PScope4\\WY2017_AVO_PScope4\\images\\use"
+folder = "D:\\IMAGES\\AVO_Images\\AVO_PScope4\\WY2016_AVO_PScope4\\images\\use"
 file_type = ".tif"
 plot_type = "NDVI"
+
+#ANNA: PScope 16-18, NDWI NDVI, clip to extents (to be sent)
+# Reclassifying photo at .05 breaks  <-- Ask Josh about this?
+# Reclassifying photo at binary (after threshold for wetness) >= 0, < 0   <-- Ask Josh about this?
+# Greater than 0, wet, less than 0, dry. Get sum of wet.
+
+
+# --- At break == 0, not picking up Vernal Pools, picking up stock ponds
 
 ## Enters defined Folder
 setwd(folder);
@@ -45,10 +53,17 @@ lapply(files, function(file) {
   outfile <- writeRaster(output, filename=paste(file,"-",plot_type,file_type, sep=""), format="GTiff", overwrite=TRUE,options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
   
   ## Plot to PDF
-  pdf(paste(file,"-",plot_type,".pdf", sep=""))
-  plot(output)
-  dev.off()
+  #pdf(paste(file,"-",plot_type,".pdf", sep=""))
+  #plot(output)
+  #dev.off()
 })
+
+##getting bounds for each tiff
+##applying a threshold (what is "water"? 1, otherwise "dry" 0)
+##combine all tiffs of 
+
+
+
 
 
 
